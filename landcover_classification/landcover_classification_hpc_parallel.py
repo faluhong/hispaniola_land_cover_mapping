6cefb8386171bb80385ead59e6785ff5d9824429
falu <faluhong@gmail.com>
2024-07-17 00:51:26 +0800

    update the MMU function and add the pyramids and color table function

--- landcover_classification/landcover_classification_hpc_parallel.py
+++ landcover_classification/landcover_classification_hpc_parallel.py
@@ -1,6 +1,7 @@
 """
     conduct the land cover classification in parallel mode in HPC
-    The basic idea is conducting the classification for each block in parallel mode. After all blocks are classified, merge the land cover and output
+    The basic idea is conducting the classification for each block in parallel mode.
+    After all blocks are classified, merge the land cover and output
 """
 
 import time
@@ -11,28 +12,20 @@ import sys
 import glob
 import pandas as pd
 import joblib
-from datetime import datetime
-import matplotlib.pyplot as plt
-import json as js
 from osgeo import gdal, gdal_array, gdalconst
-import sklearn
-from sklearn.ensemble import RandomForestClassifier
-import heapq
 import click
-import fiona
 import logging
 from scipy.ndimage import label, generate_binary_structure
+from skimage.morphology import erosion
 
 pwd = os.getcwd()
 rootpath = os.path.abspath(os.path.join(pwd, '..'))
-path_pythoncode = join(rootpath, 'pythoncode')
-sys.path.append(path_pythoncode)
 
-from landcover_classification import (get_output_rootpath, get_output_path,
-                                      read_dem, read_cold_reccg,
-                                      land_cover_classification,
-                                      img_lcmap_block_acquire, landcover_fill, landcover_merge,
-                                      get_projection_info, landcover_output)
+from landcover_classification.lc_classification import (get_output_rootpath, get_output_path,
+                                                        read_dem, read_cold_reccg,
+                                                        land_cover_classification,
+                                                        img_lcmap_block_acquire, landcover_fill, landcover_merge,
+                                                        get_projection_info, landcover_output)
 
 NBands, NRow, NCol = 8, 2500, 2500
 Block_Size = 250
@@ -41,6 +34,7 @@ record_start_year = 1984
 record_end_year = 2022
 
 list_year = np.arange(record_start_year, record_end_year + 1)
+
 
 def get_all_task():
     """
@@ -74,8 +68,6 @@ def get_finished_task(landcover_version):
     Returns:
         df: The dataframe containing the tile name and block name for the finished blocks
     """
-
-    list_tilename = ['h05v02']
 
     list_finished_tile = os.listdir(join(rootpath, 'results', '{}_landcover_classification'.format(landcover_version)))
 
@@ -167,34 +159,31 @@ def mosaic_land_cover_map(landcover_version):
     return img_lcmap
 
 
-def mmu_filter(img_lc, mmu, eight_connected):
+def mmu_erosion_process(img_lc, cross=np.ones((3, 3)), connection_structure=generate_binary_structure(2, 2)):
     """
-        using the MMU to filter the primary forest
-        (1) combine the primary wet and dry forests as primary forest
-        (2) apply the MMU and convert the primary forest into secondary forest
+        Define the Minimum Mapping Unit (MMU) for the primary forest pixels
+
+        The PF clusters need to have the structure of 3X3 to be considered as the primary forest
+
+        Args:
+            img_lc: the land cover map
+            cross: the structure of the erosion process, default is 3X3
+            connection_structure: the structure of the connected pixels, default is 8-connected
     """
 
-    pf_mask = (img_lc == 3) | (img_lc == 4)  # extract the primary forest mask
+    pf_mask = (img_lc == 2) | (img_lc == 3)  # extract the primary forest mask
+
+    img_core_pf = erosion(pf_mask, cross)  # get the PF pixels that has the 5X5 core area
 
     # using scipy.ndimage.label to get the clustered primary forest
-    img_labelled_cluster, num_features = label(pf_mask, structure=eight_connected)
+    img_labelled_cluster, num_features = label(pf_mask, structure=connection_structure)  # get the primary forest cluster with each cluster labelled
 
-    # get the count of each cluster size
-    cluster_label, cluster_counts = np.unique(img_labelled_cluster, return_counts=True)
-    cluster_label_cal = cluster_label[1::]  # exclude the non-primary-forest pixel
-    cluster_counts_cal = cluster_counts[1::]  # exclude the non-primary-forest pixel
+    label_cluster_with_core_pf = img_labelled_cluster[img_core_pf]  # get the labels of clusters which contain the core primary forest structure
 
-    cluster_count_less_than_threshold = cluster_counts_cal <= mmu
-
-    cluster_label_mask = cluster_label_cal[cluster_count_less_than_threshold]
-
-    mask = np.isin(img_labelled_cluster, cluster_label_mask)
-    mask[img_labelled_cluster == 0] = False
+    mask_cluster_with_core_pf = np.isin(img_labelled_cluster, label_cluster_with_core_pf)  # mask to get the PF clusters contain the core PF structure
 
     img_lc_filter = img_lc.copy()
-    img_lc_filter[mask] = 5  # set the primary forest patches that are less than the MMU as secondary forest
-
-    img_lc_filter = img_lc_filter.astype(int)
+    img_lc_filter[(pf_mask) & (~mask_cluster_with_core_pf)] = 4  # convert the PF clusters that don't contain the core PF structure into SF
 
     return img_lc_filter
 
@@ -219,7 +208,9 @@ def mosaic_output(landcover_version, src_geotrans, dst_proj, img_lcmap, year):
 
     filename_output = join(path_output, '{}_{:4d}_landcover.tif'.format(landcover_version, year))
 
-    tif_sat_temp = gdal.GetDriverByName('GTiff').Create(filename_output, ncols, nrows, 1, gdalconst.GDT_Byte)
+    tif_sat_temp = gdal.GetDriverByName('GTiff').Create(filename_output, ncols, nrows, 1,
+                                                        gdalconst.GDT_Byte,
+                                                        options=["COMPRESS=LZW"])
     tif_sat_temp.SetGeoTransform(src_geotrans)
     tif_sat_temp.SetProjection(dst_proj)
 
@@ -228,18 +219,65 @@ def mosaic_output(landcover_version, src_geotrans, dst_proj, img_lcmap, year):
 
     del tif_sat_temp
 
+    return filename_output
+
+
+def add_pyramids_color_in_lc_tif(filename_tif, list_overview=None):
+    """
+        add pyramids and color table in the tif file
+    """
+
+    if list_overview is None:
+        list_overview = [2, 4, 8, 16, 32, 64]
+
+    colors = np.array([np.array([241, 1, 0, 255]),        # Developed
+                       np.array([29, 101, 51, 255]),      # Primary wet forest
+                       np.array([208, 209, 129, 255]),    # Primary dry forest
+                       np.array([108, 169, 102, 255]),    # Secondary forest
+                       np.array([174, 114, 41, 255]),     # Shrub/Grass
+                       np.array([72, 109, 162, 255]) ,     # Water
+                       np.array([200, 230, 248, 255]),    # Wetland
+                       np.array([179, 175, 164, 255]),    # Other
+                       ])
+
+    dataset = gdal.Open(filename_tif, gdal.GA_Update)
+
+    # Generate overviews/pyramids
+    # The list [2, 4, 8, 16, 32] defines the downsampling factors for the overviews
+    dataset.BuildOverviews(overviewlist=list_overview)
+
+    # Get the first band of the image
+    band = dataset.GetRasterBand(1)
+
+    # Create a new color table
+    color_table = gdal.ColorTable()
+
+    # Set the color for each value in the color table
+    for i in range(0, len(colors)):
+        if i == 0:
+            color_table.SetColorEntry(0, (255, 255, 255, 255))
+        else:
+            color_table.SetColorEntry(i, tuple(colors[i - 1]))
+
+    # Assign the color table to the band
+    band.SetRasterColorTable(color_table)
+
+    # Save the changes and close the dataset
+    dataset = None
+
+    return None
 
 @click.command()
 @click.option('--rank', type=int, default=0, help='rank  $SLURM_ARRAY_TASK_ID')
 @click.option('--n_cores', type=int, default=1, help='the total applied cores   $SLURM_ARRAY_TASK_MAX')
-@click.option('--landcover_version', type=str, default='v1', help='output path version flag')
-@click.option('--post_processing_flag', type=int, default=1, help='output path version flag')
+@click.option('--landcover_version', type=str, default='v1', help='land cover version')
+@click.option('--post_processing_flag', type=int, default=1, help='post-processing flag')
 def main(rank, n_cores, landcover_version, post_processing_flag):
 
     if post_processing_flag == 0:
         post_processing_des = 'No rule applied'
     elif post_processing_flag == 1:
-        post_processing_des = 'PF rule after 1996 & Develop correction 1996 and 2022 & last segment rule'
+        post_processing_des = 'PF rule after 1996 & NBR correction included & Develop correction & last segment rule'
     else:
         post_processing_des = None
 
@@ -261,13 +299,13 @@ def main(rank, n_cores, landcover_version, post_processing_flag):
 
         dst_geotrans = (2708008.6177587425336242, 30.0, 0.0, -140855.6491435449570417, 0.0, -30.0)
 
-        dst_proj = 'PROJCS["Albers_Conic_Equal_Area",GEOGCS["WGS 84",DATUM["WGS_1984",' \
-                   'SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],' \
-                   'AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0],UNIT["Degree",0.0174532925199433]],' \
-                   'PROJECTION["Albers_Conic_Equal_Area"],' \
-                   'PARAMETER["latitude_of_center",23],PARAMETER["longitude_of_center",-96],PARAMETER["standard_parallel_1",29.5],' \
-                   'PARAMETER["standard_parallel_2",45.5],PARAMETER["false_easting",0],PARAMETER["false_northing",0],' \
-                   'UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH]]'
+        dst_proj = ('PROJCS["Albers_Conic_Equal_Area",GEOGCS["WGS 84",DATUM["WGS_1984",' 
+                   'SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],' 
+                   'AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0],UNIT["Degree",0.0174532925199433]],' 
+                   'PROJECTION["Albers_Conic_Equal_Area"],' 
+                   'PARAMETER["latitude_of_center",23],PARAMETER["longitude_of_center",-96],PARAMETER["standard_parallel_1",29.5],' 
+                   'PARAMETER["standard_parallel_2",45.5],PARAMETER["false_easting",0],PARAMETER["false_northing",0],' 
+                   'UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH]]')
 
         # mosaic the output land cover
         img_lcmap = mosaic_land_cover_map(landcover_version)
@@ -276,11 +314,15 @@ def main(rank, n_cores, landcover_version, post_processing_flag):
             img_lcmap_eachyear = img_lcmap[i_year, :, :].copy()
 
             # using the Minimum Mapping Unit to remove the scattered primary forest pixels
-            img_lcmap_eachyear = mmu_filter(img_lcmap_eachyear, mmu=5, eight_connected=generate_binary_structure(2, 2))
+            img_lcmap_eachyear = mmu_erosion_process(img_lcmap_eachyear, cross=np.ones((3, 3)),
+                                                     connection_structure=generate_binary_structure(2, 2))
             img_lcmap[i_year, :, :] = img_lcmap_eachyear
 
             # output the annual land cover map
-            mosaic_output(landcover_version, dst_geotrans, dst_proj, img_lcmap_eachyear, year)
+            filename_output = mosaic_output(landcover_version, dst_geotrans, dst_proj, img_lcmap_eachyear, year)
+
+            # add pyramids and color table in the mosaiced tif file
+            add_pyramids_color_in_lc_tif(filename_output)
 
     else:
         offset_flag = 1  # the first rank is used for merge the land cover, an offset is needed to allocate the remaining cores
@@ -313,7 +355,7 @@ def main(rank, n_cores, landcover_version, post_processing_flag):
                 output_path = get_output_path(landcover_version, tilename)
 
                 # read the pre-trained random forest
-                output_name_rf = join(rootpath, 'data', 'random_forest_model', 'rf_classifier_i01.joblib')
+                output_name_rf = join(rootpath, 'results', 'rf_output', 'rf_classifier_i01.joblib')
                 rf_classifier = joblib.load(output_name_rf)
 
                 # read topography information
@@ -333,7 +375,7 @@ def main(rank, n_cores, landcover_version, post_processing_flag):
                 # fill the missing values
                 img_lcmap_fill = landcover_fill(img_lcmap_eachblock)
 
-                # merge secondary wet and secondary dry forests to secondary forest
+                # merge secondary wet and secondary dry forests; merge barren and cropland to other
                 img_lcmap_merge = landcover_merge(img_lcmap_fill)
 
                 # output the land cover map
